4fccba52b2dd3821edf3f89efb7638009703657c
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-02-09 04:21:54 +0000

    sort relevance by date

--- trunk/minds/search.py
+++ trunk/minds/search.py
@@ -17,31 +17,47 @@ log = logging.getLogger('search')
 
 class MatchItem:
     def __init__(self, hits, key):
-        self.index = 0
-        self.doc = ''
-        self.score = 0
-        self.docid = ''
-        self.zfile = None
-        self.fp = None
+
+        # lucene data
+        self.index      = key   # hits index
+        self.id         = 0     # lucene id
+        self.doc        = ''
+        self.score      = 0
+
+        # doument meta-data
+        self.docid      = ''
+        self.date       = ''
+        self.uri        = ''
+        self.title      = ''
+        self.description= ''
+
         self.load(hits, key)
 
     def load(self, hits, key):
-        self.index      = key
-        self.doc        = hits.doc(key)
-        self.score      = hits.score(key)
+        self.id         = hits[key][1]
+        self.doc        = hits[key][2]
+        self.score      = hits[key][0]
+        self.score0     = hits[key][3]
+#        self.doc        = hits.doc(key)
+#        self.score      = hits.score(key)
         self.docid      = self.doc.get('docid'      )
         self.date       = self.doc.get('date'       )
         self.uri        = self.doc.get('uri'        )
-        self.title      = self.doc.get('title'      )
-        self.description= self.doc.get('description')
+        #self.title      = self.doc.get('title'      )
+        #self.description= self.doc.get('description')
 
     def highlight(self, analyzer, highlighter):
         maxNumFragmentsRequired = 2
-        fp = docarchive.get_document(self.docid)
-        meta, content = distillparse.parseDistillML(fp)
-        tokenStream = analyzer.tokenStream('content', StringIO.StringIO(content))
-        self.description = highlighter.getBestFragments(tokenStream, content, maxNumFragmentsRequired, "...")
-        self.title = meta.get('title','')
+        try:
+            fp = docarchive.get_document(self.docid)
+        except Exception, e:
+            # maybe the index is outdate to refer to some non-exist file
+            log.exception('Unable to get "%s"' % self.docid)
+        else:
+            meta, content = distillparse.parseDistillML(fp)
+            tokenStream = analyzer.tokenStream('content', StringIO.StringIO(content))
+            self.description = highlighter.getBestFragments(tokenStream, content, maxNumFragmentsRequired, "...")
+            self.title = meta.get('title','')
 
 
 
@@ -56,6 +72,30 @@ def parseQuery(phrase):
     return query
 
 
+MAXRESULT = 1000
+
+def sortHits(hits, maxDoc):
+
+    hitList = []
+    for i in xrange(min(1000,hits.length())):
+        id = hits.id(i)
+        score = hits.score(i)
+        date_adjusted = score / (maxDoc - id)
+        hitList.append((date_adjusted, id, hits.doc(i), score))
+
+    if not hitList:
+        return []
+
+    hitList.sort(reverse=True)
+
+    # normalize score
+    high_score = hitList[0][0]
+    hitList = [(s/high_score, id, doc, score0) for s,id,doc,score0 in hitList]
+
+    return hitList
+
+
+
 def search(query, start, end):
 
     # search
@@ -63,6 +103,8 @@ def search(query, start, end):
     searcher = lucene_logic.Searcher(pathname=dbindex)
     query = query.rewrite(searcher.reader.reader)
     hits = searcher.search(query)
+
+    hitList = sortHits(hits, searcher.reader.maxDoc()+2000)
 
     # prepare for highlighter
     formatter = SimpleHTMLFormatter("<span class='highlight'>", "</span>")
@@ -75,8 +117,12 @@ def search(query, start, end):
     for i in xrange(start,end):
         if i >= hits.length():
             break
-        item = MatchItem(hits, i)
+        item = MatchItem(hitList, i)
         item.highlight(analyzer, highlighter)
+
+        #item.explaination = str(searcher.explain(query, hits.id(i))) ###
+        item.explaination = str(searcher.explain(query, item.id)) ###
+
         result.append(item)
     searcher.close()
     return hits.length(), result
@@ -84,6 +130,43 @@ def search(query, start, end):
 
 
 ### Testing ############################################################
+
+def encode(s):
+    return s.encode('cp437','replace')      # windows console
+
+
+def run_search(querystring, start):
+
+    query = parseQuery(querystring)
+
+    print '\n-----------------------------------'
+    print "Searching for:", querystring
+    print "Query: " + str(query)
+    print '-----------------------------------'
+
+    length, result = search(query, start, start+10)
+
+    for item in result:
+        print '\n%-2d' % (item.index+1),
+
+        title = item.title and item.title or 'Untitled'
+        print encode(title[:100])
+
+        if item.description:
+            description = ' '.join(item.description.split())
+            print '  ', encode(description)
+
+        print '   %s %0.5f %0.5f %s' % (item.id, item.score, item.score0, item.date.replace('T', ' ')),
+
+        print '\n\n%s' % item.explaination ###
+
+        if item.uri:
+            print encode(item.uri[:100])
+        else:
+            print 'Address unknown'
+
+    print "\nFrom %s of %s documents found." % (start, length)
+
 
 def run():
 
@@ -117,34 +200,10 @@ def run():
         if not querystring or start < 0:
             continue
 
-        print
-        print "Searching for:", querystring
-
-        query = parseQuery(querystring)
-        length, result = search(query, start, start+10)
-
         lastQuery = querystring
         lastStart = start
 
-        for item in result:
-            print '\n%-2d' % (item.index+1),
-
-            title = item.title and item.title or 'Untitled'
-            print title.encode('latin-1','replace')[:100]
-
-            if item.description:
-                description = ' '.join(item.description.split())[:200]
-                print '  ', description.encode('latin-1','replace')
-
-            print '   %s' % item.date.replace('T', ' '),
-
-            if item.uri:
-                print item.uri.encode('latin-1','replace')[:100]
-            else:
-                print 'Address unknown'
-
-        print
-        print "%s/%s documents found." % (start, length)
+        run_search(querystring, start)
 
 
 def main(argv):

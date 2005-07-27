f78989166a05e3ae01f762ab71bad60d94cac126
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-07-27 16:44:58 +0000

    added logging

--- trunk/minds/weblib/store.py
+++ trunk/minds/weblib/store.py
@@ -1,10 +1,13 @@
 """ Define load() and save() to as the system persistence method.
 """
 
+import logging
 import sys
 
 from minds.weblib import minds_lib
 from minds.weblib import opera_lib
+
+log = logging.getLogger('weblib')
 
 def_store = minds_lib
 
@@ -21,25 +24,32 @@ def _run_close(fp, func, *args):
     finally:
         fp.close()       
 
+
 def load(filename=WEBLIB_FILENAME ):
     fp = file(filename,'rb')
-    return _run_close(fp, def_store.load, fp)
+    wlib = _run_close(fp, def_store.load, fp)
+    log.debug('Loaded %s items:%s,%s', filename, len(wlib.labels), len(wlib.webpages))
+    return wlib
 
     
 def save(wlib, filename=WEBLIB_FILENAME):
     fp = file(filename,'wb')            
     _run_close(fp, def_store.save, fp, wlib)
+    log.debug('Saved %s items:%s,%s', filename, len(wlib.labels), len(wlib.webpages))
 
 
 def load_opera(filename=OPERA_FILENAME):
     fp = file(filename,'rb')
-    return _run_close(fp, opera_lib.load, fp)
+    wlib = _run_close(fp, opera_lib.load, fp)
+    log.debug('Loaded %s items:%s,%s', filename, len(wlib.labels), len(wlib.webpages))
+    return wlib
 
     
 def main(argv):
     # put the script here
     wlib = load_opera()
     save(wlib)
+
 
 if __name__ =='__main__':
     main(sys.argv)

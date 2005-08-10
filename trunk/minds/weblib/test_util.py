1d0a0729cfe7fc2946ab26e1a33fe5e6e8600d43
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-08-10 01:36:18 +0000

    added test IndentedTextParsing

--- trunk/minds/weblib/test_util.py
+++ trunk/minds/weblib/test_util.py
@@ -2,6 +2,10 @@ import unittest
 
 import util
 
+
+#-----------------------------------------------------------------------
+# Id keyed list
+
 class TestItem(object):
     
     # for generating unique names
@@ -151,6 +155,75 @@ class TestIdNameList(TestIdList):
         super(TestIdNameList,self).test_remove()
 
 
+#-----------------------------------------------------------------------
+# Indented text parsing        
+
+class TestIndentedTextParsing(unittest.TestCase):
+    
+    DATA = """
+        C1
+            C11
+            C12
+                C121
+                C122
+            C13
+        C2
+            C21
+                C211
+        C3
+            C31                   
+    """
+
+    def test0(self):
+        tree = util.parse_indent_text('')
+        self.assertEqual(tree, ('',[]))
+        
+        tree = util.parse_indent_text('\n')
+        self.assertEqual(tree, ('',[]))
+
+    def test_simple(self):
+        tree = util.parse_indent_text(self.DATA)
+        expected = ('', [
+        ('C1', [
+            ('C11',[]),
+            ('C12',[
+                ('C121',[]),
+                ('C122',[]),
+            ]),
+            ('C13',[]),
+        ]),
+        ('C2', [
+            ('C21', [
+                ('C211',[]),
+            ]),
+        ]),
+        ('C3', [
+            ('C31',[]),
+        ]),
+        ])
+        self.assertEqual(tree, expected)
+
+
+    def test_dfs(self):
+        tree = util.parse_indent_text(self.DATA)
+        result = [(node[0], level) for node,level in util.dfs(tree)]
+        expected = [
+            ('',0),
+            ('C1',1),
+            ('C11',2),
+            ('C12',2),
+            ('C121',3),
+            ('C122',3),
+            ('C13',2),
+            ('C2',1),
+            ('C21',2),
+            ('C211',3),
+            ('C3',1),
+            ('C31',2),
+        ]
+        self.assertEqual(result, expected)
+        
+        
 if __name__ =='__main__':
     unittest.main()
         

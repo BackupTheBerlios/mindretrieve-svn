529d193b542124c29edd88d3a034e51da6282000
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-09-18 16:22:44 +0000

    added listdir(). spin off test.

--- trunk/minds/util/fileutil.py
+++ trunk/minds/util/fileutil.py
@@ -1,7 +1,20 @@
 """Utility to create file like objects
 """
 
+import os
 import StringIO
+
+def listdir(path, name_filter=None):
+    """ A custom version of os.listdir that takes regex filter. """
+    
+    # note 2005-09-18: path.listdir() is not compatible with
+    # os.listdir() because it concat basedir with files. Arguably useful
+    # in many situation but requires more code change for us.
+    if filter:
+        return filter(name_filter.match, os.listdir(path))
+    else:
+        return os.listdir(path)
+
 
 class aStringIO(StringIO.StringIO):
     """ Some customerizations the StringIO module to provides the 'closed' attribute """
@@ -146,76 +159,3 @@ class FileSocket:
         ''' a reasonable action '''
         self.fin.close()
         self.fout.close()
-
-
-
-
-
-### Testing ############################################################
-
-import unittest
-
-class TestBoundedFile(unittest.TestCase):
-
-  def test_boundedFile(self):
-    print '\n@test_boundedFile'
-    buf = StringIO.StringIO()
-    bfp = BoundedFile(buf, 10)
-
-    bfp.write('12345')
-    self.assert_(not bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 5)
-
-    bfp.write('678')
-    self.assert_(not bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 8)
-
-    bfp.write('90ab')
-    self.assert_(bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 8)
-
-    # although bfp is still 2 characters below maxsize
-    # make sure once it is overflowed you cannot write anymore
-    bfp.write('c')
-    self.assert_(bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 8)
-
-
-  def test_boundedFile1(self):
-    print '\n@test_boundedFile1'
-    buf = StringIO.StringIO()
-    bfp = BoundedFile(buf, 10)
-
-    # check boundary condition when cursize == maxsize
-    bfp.write('1234567890')
-    self.assert_(not bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 10)
-
-    bfp.write('a')
-    self.assert_(bfp.isOverflow())
-    self.assertEqual(len(buf.getvalue()), 10)
-
-
-  def test_boundedFileDelegation(self):
-    print '\n@test_boundedFileDelegation'
-    buf = StringIO.StringIO()
-    bfp = BoundedFile(buf, 10)
-
-    bfp.write('1234567890')
-    self.assertEqual(bfp.getvalue(), '1234567890')
-
-
-
-
-class TestFileUtil(unittest.TestCase):
-
-  def test_RecordFile(self):
-    print '\n@test_RecordFile'
-    f1 = StringIO.StringIO("""line1\r\nline2\r\nline3\n""")
-    rec = StringIO.StringIO()
-    for i, line in enumerate(RecordFile(f1,rec)):
-        print 'line %d: [%s]' % (i, line)
-    self.assertEqual(f1.getvalue(), rec.getvalue())
-
-if __name__ == '__main__':
-    unittest.main()

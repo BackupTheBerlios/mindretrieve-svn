a506da14e26a62842ed04940dd3e9b72a56b4872
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-10-04 16:26:02 +0000

    added rename()

--- trunk/minds/weblib/test/test_util.py
+++ trunk/minds/weblib/test/test_util.py
@@ -7,7 +7,7 @@ from minds.weblib import util
 # Id keyed list
 
 class TestItem(object):
-    
+
     # for generating unique names
     nameId = 1
 
@@ -17,23 +17,23 @@ class TestItem(object):
             name = 'item%s' % TestItem.nameId
             TestItem.nameId += 1
         self.name = name
-        
+
     def __str__(self):
         return self.name
-        
-            
+
+
 class TestIdList(unittest.TestCase):
-    
+
     def setUp(self):
         TestItem.nameId = 1
         self.lst = util.IdList()
-        
+
     def test0(self):
         self.assertEqual(len(self.lst), 0)
         for i in self.lst:
             self.fail('expect empty')
-        
-            
+
+
     def test3(self):
         item1 = TestItem()
         item2 = TestItem()
@@ -41,19 +41,19 @@ class TestIdList(unittest.TestCase):
         self.lst.append(item1)
         self.lst.append(item2)
         self.lst.append(item3)
-        
-        self.assertEqual(len(self.lst), 3)
-        self.assertEqual(self.lst.getById(0), None)    
-        self.assertEqual(self.lst.getById(1), item1)    
-        self.assertEqual(self.lst.getById(2), item2)    
-        self.assertEqual(self.lst.getById(3), item3)    
-        self.assertEqual(self.lst.getById(4), None)    
-        self.assertEqual(self.lst._lastId, 3)        
 
-        # test __iter__       
-        enum = [(item.id, item) for item in self.lst]       
+        self.assertEqual(len(self.lst), 3)
+        self.assertEqual(self.lst.getById(0), None)
+        self.assertEqual(self.lst.getById(1), item1)
+        self.assertEqual(self.lst.getById(2), item2)
+        self.assertEqual(self.lst.getById(3), item3)
+        self.assertEqual(self.lst.getById(4), None)
+        self.assertEqual(self.lst._lastId, 3)
+
+        # test __iter__
+        enum = [(item.id, item) for item in self.lst]
         expected = zip(range(1,4), [item1, item2, item3])
-        self.assertEqual(enum, expected)    
+        self.assertEqual(enum, expected)
 
 
     def test_with_id(self):
@@ -63,19 +63,19 @@ class TestIdList(unittest.TestCase):
         self.lst.append(item1)
         self.lst.append(item2)
         self.lst.append(item3)
-        
+
         self.assertEqual(len(self.lst), 3)
-        self.assertEqual(self.lst.getById(0), None)    
-        self.assertEqual(self.lst.getById(1), None)    
-        self.assertEqual(self.lst.getById(11), item1)    
-        self.assertEqual(self.lst.getById(12), item2)    
-        self.assertEqual(self.lst.getById(13), item3)    
+        self.assertEqual(self.lst.getById(0), None)
+        self.assertEqual(self.lst.getById(1), None)
+        self.assertEqual(self.lst.getById(11), item1)
+        self.assertEqual(self.lst.getById(12), item2)
+        self.assertEqual(self.lst.getById(13), item3)
         self.assertEqual(self.lst._lastId, 13)
-       
-        # test __iter__       
-        enum = [(item.id, item) for item in self.lst]       
+
+        # test __iter__
+        enum = [(item.id, item) for item in self.lst]
         expected = zip(range(11,14), [item1, item2, item3])
-        self.assertEqual(enum, expected)    
+        self.assertEqual(enum, expected)
 
 
     def test_duplicates(self):
@@ -89,7 +89,7 @@ class TestIdList(unittest.TestCase):
         # reject duplicated item2
         self.assertRaises(KeyError, self.lst.append, item2)
         self.assertEqual(len(self.lst), 1)
-        
+
     def test_remove(self):
         item1 = TestItem()
         item2 = TestItem()
@@ -97,30 +97,30 @@ class TestIdList(unittest.TestCase):
         self.lst.append(item1)
         self.lst.append(item2)
         self.lst.append(item3)
-        
+
         # originally has 3 items
         self.assertEqual(len(self.lst), 3)
-        self.assertEqual(self.lst.getById(1), item1)    
+        self.assertEqual(self.lst.getById(1), item1)
 
         self.lst.remove(item1)
-        
+
         # only 2 items remain
         self.assertEqual(len(self.lst), 2)
-        self.assertEqual(self.lst.getById(1), None)    
-        enum = [(item.id, item) for item in self.lst]       
+        self.assertEqual(self.lst.getById(1), None)
+        enum = [(item.id, item) for item in self.lst]
         expected = zip(range(2,4), [item2, item3])
-        self.assertEqual(enum, expected)    
-        
+        self.assertEqual(enum, expected)
+
         # can't delete this again
         self.assertRaises(KeyError, self.lst.remove, item1)
         self.assertEqual(len(self.lst), 2)
-                
-        
+
+
 class TestIdNameList(TestIdList):
 
     def setUp(self):
         TestItem.nameId = 1
-        self.lst = util.IdNameList()        
+        self.lst = util.IdNameList()
 
 
     def test3(self):
@@ -133,12 +133,19 @@ class TestIdNameList(TestIdList):
         self.assertEqual(self.lst.getByName('not exist'), None)
 
 
+    def test_blank(self):
+        blank_item = TestItem(100, '')
+        # erase generated name
+        blank_item.name = ''
+        self.assertRaises(KeyError, self.lst.append, blank_item)
+
+
     def test_duplicates(self):
         super(TestIdNameList,self).test_duplicates()
 
-        # flush the list 
+        # flush the list
         self.lst = util.IdNameList()
-        
+
         item1 = TestItem(1, 'same')
         item2 = TestItem(2, 'same')
 
@@ -151,79 +158,44 @@ class TestIdNameList(TestIdList):
         self.assertEqual(len(self.lst), 1)
 
 
+    def test_rename(self):
+        # insert 'item1'
+        item1 = TestItem()
+        self.lst.append(item1)
+
+        # rename item1
+        gbn = self.lst.getByName
+        item = gbn('item1')
+        self.assertEqual(item, item1)
+        self.lst.rename(item, 'newName')
+
+        self.assertEqual(gbn('item1'), None)            # old name is gone
+        self.assertEqual(gbn('newName'), item)          # new name refers to former item1
+        self.assertEqual(gbn('NEWNAME'), item)          # mix case should match
+        self.assertEqual(gbn('newName').name, 'newName')# name is changed
+
+
+    def test_invalid_rename(self):
+        # insert 'item1' and 'item2'
+        item1 = TestItem()
+        item2 = TestItem()
+        self.lst.append(item1)
+        self.lst.append(item2)
+        self.assertTrue(self.lst.getByName('item1'))
+        self.assertTrue(self.lst.getByName('item2'))
+
+        # rename item1 to item2
+        item = self.lst.getByName('item1')
+        self.assertRaises(KeyError, self.lst.rename, item, 'item2')
+
+        # rename item1 to blank
+        self.assertRaises(KeyError, self.lst.rename, item, '')
+
+
     def test_remove(self):
         super(TestIdNameList,self).test_remove()
 
 
-#-----------------------------------------------------------------------
-# Indented text parsing        
 
-class TestIndentedTextParsing(unittest.TestCase):
-    
-    DATA = """
-        C1
-            C11
-            C12
-                C121
-                C122
-            C13
-        C2
-            C21
-                C211
-        C3
-            C31                   
-    """
-
-    def test0(self):
-        tree = util.parse_indent_text('')
-        self.assertEqual(tree, ('',[]))
-        
-        tree = util.parse_indent_text('\n')
-        self.assertEqual(tree, ('',[]))
-
-    def test_simple(self):
-        tree = util.parse_indent_text(self.DATA)
-        expected = ('', [
-        ('C1', [
-            ('C11',[]),
-            ('C12',[
-                ('C121',[]),
-                ('C122',[]),
-            ]),
-            ('C13',[]),
-        ]),
-        ('C2', [
-            ('C21', [
-                ('C211',[]),
-            ]),
-        ]),
-        ('C3', [
-            ('C31',[]),
-        ]),
-        ])
-        self.assertEqual(tree, expected)
-
-
-    def test_dfs(self):
-        tree = util.parse_indent_text(self.DATA)
-        result = [(node[0], level) for node,level in util.dfs(tree)]
-        expected = [
-            ('',0),
-            ('C1',1),
-            ('C11',2),
-            ('C12',2),
-            ('C121',3),
-            ('C122',3),
-            ('C13',2),
-            ('C2',1),
-            ('C21',2),
-            ('C211',3),
-            ('C3',1),
-            ('C31',2),
-        ]
-        self.assertEqual(result, expected)
-        
-        
 if __name__ =='__main__':
     unittest.main()
-        
--- trunk/minds/weblib/util.py
+++ trunk/minds/weblib/util.py
@@ -13,20 +13,20 @@ def diff(a,b):
 # Id keyed list
 
 class IdList(object):
-    """ A container keyed by id """    
-    
+    """ A container keyed by id """
+
     def __init__(self):
         self._lastId = 0
         self._lst = []
         self._id2item = {}
-            
+
     def append(self, item):
-        if item.id < 0:                
+        if item.id < 0:
             # generate new id
             self._lastId += 1
             item.id = self._lastId
-            
-        elif item.id > self._lastId:   
+
+        elif item.id > self._lastId:
             # id supplied, maintain self._lastId
             self._lastId = item.id
 
@@ -47,39 +47,52 @@ class IdList(object):
         except ValueError, e:
             raise KeyError(e)     # make it an IndexError
         del self._id2item[item.id]
-        
+
     def __len__(self):
         return len(self._lst)
-    
+
     def __iter__(self):
         return iter(self._lst)
 
 
 class IdNameList(IdList):
-    """ A container keyed by id and name. """    
-    
+    """ A container keyed by id and name. """
+
     def __init__(self):
         super(IdNameList, self).__init__()
-        self._name2item = {}        
-    
+        self._name2item = {}
+
     def append(self, item):
         lname = item.name.lower()
-        if self._name2item.has_key(lname):
-            raise KeyError('Duplicated %s name "%s"' % (unicode(item), lname))
+        if not lname or self._name2item.has_key(lname):
+            raise KeyError('Duplicated or invalid name "%s" tag %s' % (lname, unicode(item)))
         super(IdNameList, self).append(item)
         self._name2item[lname] = item
 
     def getByName(self, name):
-        """ 
-        @param name - would turn into lower case before lookup.
-        @return item or None if not found 
         """
-        lname = name.lower()        
+        @param name - would turn into lower case before lookup.
+        @return item or None if not found
+        """
+        lname = name.lower()
         return self._name2item.get(lname, None)
-        
+
     def remove(self, item):
         """ raise KeyError if item is not in the list """
         del self._name2item[item.name.lower()]
         super(IdNameList, self).remove(item)
-        
-        
+
+    def rename(self, item, newName):
+        # rename need to takes care of self._name2item
+        lNewName = newName.lower()
+        if not lNewName or self._name2item.has_key(lNewName):
+            raise KeyError('Duplicated or invalid name "%s", fail to rename %s' % (lNewName, unicode(item)))
+        try:
+            lname = item.name.lower()
+            del self._name2item[lname]
+        except ValueError:
+            pass # should not happen
+        item.name = newName
+        self._name2item[lNewName] = item
+
+

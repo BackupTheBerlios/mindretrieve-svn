628b18700240ba59a2aaab48525e6fa469ddd25f
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-10-13 20:39:36 +0000

    added some test cases but need to look at failed cases.

--- trunk/minds/cgibin/test/test_weblib_cgi.py
+++ trunk/minds/cgibin/test/test_weblib_cgi.py
@@ -16,8 +16,8 @@ weblib_path = testcfg.getpath('weblib')
 class TestWeblibCGI(unittest.TestCase):
 
   def setUp(self):
-    # we're going to overwrite file in weblib_path, make sure it is test.  
-    assert 'test' in weblib_path 
+    # we're going to overwrite file in weblib_path, make sure it is test.
+    assert 'test' in weblib_path
     # load the test weblib.dat
     from minds.weblib import store
     testfile_path = testpath/'test_weblib/weblib.dat'
@@ -33,10 +33,7 @@ class TestWeblibCGI(unittest.TestCase):
     p = patterns_tester.checkPatterns(buf, patterns, no_pattern)
     msg = (p == no_pattern) and 'unexpected pattern found' or 'pattern missing'
     self.assert_(not p,
-        'failed:%s\n  %s: %s%s' % (
-            path, 
-            msg,
-            p, 
+        'failed:%s\n  %s: %s%s' % (path, msg, p,
             patterns_tester.showFile(buf, 'out', 1024),
     ))
 
@@ -46,22 +43,18 @@ class TestWeblibCGI(unittest.TestCase):
     ])
 
     self.checkPathForPattern("/weblib?tag=inbox", [
-        '<html>', 
+        '<html>',
         'www.mindretrieve.net',     # this entry appears on main page
         '</html>',
     ])
 
     # controlled test for below
     # ja.wikipedia.org will not be listed unless there is a right query
-    self.checkPathForPattern("/weblib?tag=inbox", [
-        '<html>',
-        ], 
+    self.checkPathForPattern("/weblib?tag=inbox", ['<html>',],
         'ja.wikipedia.org'
     )
 
-    self.checkPathForPattern("/weblib?tag=English", [
-        '<html>',
-        ], 
+    self.checkPathForPattern("/weblib?tag=English", ['<html>',],
         'ja.wikipedia.org'
     )
 
@@ -69,86 +62,94 @@ class TestWeblibCGI(unittest.TestCase):
   def test_weblib_tag(self):
     # this is the URL encoded for the Japanese tag
     self.checkPathForPattern("/weblib?tag=%E6%97%A5%E6%9C%AC%E8%AA%9E", [
-        '<html>', 
-        'ja.wikipedia.org', 
-        '</html>',
+        '<html>', 'ja.wikipedia.org', '</html>',
     ])
 
 
   def test_weblib_query(self):
     self.checkPathForPattern("/weblib?query=%20-%20Wikipedia", [
-        '<html>', 
-        'ja.wikipedia.org', 
-        '</html>',
+        '<html>', 'ja.wikipedia.org', '</html>',
     ])
 
 
   def test_weblib_go(self):
     self.checkPathForPattern("/weblib/4/go;url", [
         'HTTP/1.0 302 Found',
-        'ja.wikipedia.org', 
+        'ja.wikipedia.org',
         ],
         '</html>'
     )
 
 
-  def test_weblib_form_new(self):
+  def test_GET_form_new(self):
     self.checkPathForPattern("/weblib/_", [
-        '<html>', 
-        'Add Entry',
-        '</html>',
+        '<html>', 'Add Entry', '</html>',
     ])
     self.checkPathForPattern("/weblib/_/form", [
-        '<html>', 
-        'Add Entry',
-        '</html>',
+        '<html>', 'Add Entry', '</html>',
     ])
 
-  def test_weblib_put_form_new(self):
+
+  def test_GET_form(self):
+    self.checkPathForPattern("/weblib/4", [
+        '<html>', 'Edit Entry', 'ja.wikipedia.org', '</html>',
+    ])
+    self.checkPathForPattern("/weblib/4/form", [
+        '<html>', 'Edit Entry', 'ja.wikipedia.org', '</html>',
+    ])
+
+
+  def test_PUT_form_new(self):
     wlib = store.getMainBm()
     self.assertEqual(len(wlib.webpages),5)
 
     # PUT new form
     self.checkPathForPattern("/weblib/_?method=put&filled=1&u=http%3A%2F%2Fwww.mindretrieve.net%2F&t=Test%20Title", [
-        'HTTP/1.0 302 Found', 
+        'HTTP/1.0 302 Found',
         'location: /weblib',
     ])
-    
+
     # one item has added
     self.assertEqual(len(wlib.webpages),6)
 
 
-  def test_weblib_form(self):
-    self.checkPathForPattern("/weblib/4", [
-        '<html>', 
-        'Edit Entry',
-        'ja.wikipedia.org', 
-        '</html>',
-    ])
-    self.checkPathForPattern("/weblib/4/form", [
-        '<html>', 
-        'Edit Entry',
-        'ja.wikipedia.org', 
-        '</html>',
-    ])
-
-
-  def test_weblib_put_form(self):
+  def test_PUT_form(self):
     wlib = store.getMainBm()
     self.assertEqual(len(wlib.webpages),5)
     item = wlib.webpages.getById(1)
     self.assertEqual(item.name, 'MindRetrieve - Search Your Personal Web')
 
-    # PUT form    
+    # PUT form
     self.checkPathForPattern("/weblib/1?method=put&filled=1&u=http%3A%2F%2Fwww.mindretrieve.net%2F&t=Test%20Title", [
-        'HTTP/1.0 302 Found', 
+        'HTTP/1.0 302 Found',
         'location: /weblib',
     ])
-    
+
     # one item has changed
     self.assertEqual(len(wlib.webpages),5)
     item = wlib.webpages.getById(1)
     self.assertEqual(item.name, 'Test Title')
+
+
+  def test_GET_entryOrg(self):
+    self.checkPathForPattern("/weblib.entryOrg", [
+        '<html>', 'Organize', '</html>',
+    ])
+
+
+  def test_PUT_entryOrg(self):
+    self.fail()
+
+
+  def test_GET_categorize(self):
+    self.checkPathForPattern("/weblib.categorize", [
+        '<html>', 'Tag Categories', '</html>',
+    ])
+
+
+  def test_PUT_categorize(self):
+    self.fail()
+
 
 
 if __name__ == '__main__':

df3223afb5dcfb93338027bff41201315f645dd5
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2005-10-14 21:29:52 +0000

    handle deform tree. Added edit_rename, edit_delete

--- trunk/minds/weblib/graph.py
+++ trunk/minds/weblib/graph.py
@@ -44,9 +44,9 @@ def parse_text_tree(data):
     Parse input of indented text into a graph of nodes.
     Return the root node.
 
-    A node is tuple of (name, list of children).
+    A node is a 2-tuple of [name, list of children)]
+    It is represented by list so that the elements can be updated.
     """
-    # create the root node
     root_node = ['',[]]
 #    nodes = {'': root_node}
 
@@ -63,27 +63,29 @@ def parse_text_tree(data):
 
     for line in _parse_lines(data):
         indent, name = _split_indent(line)
-        name = name.lower()
         node = [name,[]]
+#        name = name.lower()
 #        node = nodes.setdefault(name,[name,[]])
-        if indent == cur_indent:
-            # 2nd last in the stack would be the parent
-            # note: -1 < 0 <= indent == cur_indent
+
+        # Look for parent with smaller indent (i.e. cur_indent < indent)
+        # case 1: cur_indent < indent, last line is the parent, no popping necessary.
+        # case 2: cur_indent == indent, this line is a sibling of last line. One pop should find the parent
+        # case 3: cur_indent > indent, one or more pop to find the parent.
+        while cur_indent >= indent:
+            # note: -1 < 0 <= indent <= cur_indent
             # i.e. there is at least 2 items in the stack
             stack.pop()
             cur_indent, cur_node = stack[-1]
-        elif indent > cur_indent:
-            # cur_node would be the parent
-            pass
-        else:
-            # find parent with smaller indent
-            while indent <= cur_indent:
-                # note: -1 < 0 <= indent <= cur_indent
-                # i.e. there is at least 2 items in the stack
-                stack.pop()
-                cur_indent, cur_node = stack[-1]
-            # note: should check that indent == cur_indent here.
-            # otherwise the text's indentation is invalid.
+
+        # note: You may find this line has a smaller ident than its
+        # older sibling. The tree is deformed but it is reasonable
+        # to accept this defect.
+        #
+        # Example of a deformed tree:
+        #   C1
+        #   ....C11
+        #   ..C12     <-- Child of C1, it should really align with C11
+        #   ....C121  <-- this will become child of C12, not sibling of C11
 
         # append to parent
         cur_node[1].append(node)
@@ -93,6 +95,84 @@ def parse_text_tree(data):
         stack.append((cur_indent,cur_node))
 
     return root_node
+
+
+def edit_text_tree_rename(data, tag0, tag1):
+    """
+    tag0 has renamed to tag1.
+    Apply the same changed to text tree
+    """
+    tag0 = tag0.lower()
+    output = []
+    for line in data.splitlines(True):
+        if line.strip().lower() == tag0:
+            line = line.lower().replace(tag0,tag1)
+        output.append(line)
+    return ''.join(output)
+
+
+def edit_text_tree_delete(data, tag0):
+    """
+    tag0 has been deleted.
+    Apply the same changed to text tree.
+    Realign children lines if necessary.
+    """
+    tag0 = tag0.lower()
+    output = []
+
+    trim_row = None     # if in trimming state, indent of trim_row
+
+    for line in data.splitlines(True):
+        if line.isspace():
+            output.append(line)
+            continue
+
+        indent, name = _split_indent(line)
+
+        # trim_row determine if we are in trimming state
+        if trim_row != None:
+            if len(indent) <= len(trim_row):
+                # leave trim mode
+                _trim_children_and_output(len(trim_row), children, output)
+                trim_row = None
+                children = [] # tidy up
+            else:
+                # TODO: What if this line also contain tag0!!!
+                # This is an obvious cycle, shouldn't be allowed in the first place.
+                # 2005-10-14: we'll not handle this case and it should not turn out too bad.
+                children.append((len(indent),line))
+                continue
+
+        if trim_row == None:
+            if line.strip().lower() != tag0:
+                # unaffected
+                output.append(line)
+            else:
+                # enter trimming state
+                trim_row = indent
+                children = []
+
+    if trim_row:
+        _trim_children(len(trim_row), children, output)
+
+    return ''.join(output)
+
+
+def _trim_children_and_output(align_column, children, output):
+    """ Align children to align_column (shift grandchildren by same amount) """
+    if not children:
+        return
+    # use first child as child_col
+    # if the tree is welformed, this would be minimal.
+    child_col = children[0][0]
+    for col, line in children:
+        assert col > align_column
+        if col < child_col:
+            # This would happen in a deformed tree.
+            # Anyway realign to this smaller row
+            child_col = col
+        line = ' ' * (col - (child_col-align_column)) + line.lstrip()
+        output.append(line)
 
 
 def merge_DAG(p,q):
@@ -178,3 +258,23 @@ def form_cycle(p,q):
     return False
 
 
+# ------------------------------------------------------------------------
+
+DATA = """
+C1
+  C11
+     C111
+    C112x
+ C12x
+"""
+
+def main(argv):
+    print 'DATA=' + DATA
+    print
+    root = parse_text_tree(DATA)
+    for v, level in dfs(root):
+        if v:
+            print '..'*level + unicode(v)
+
+if __name__ =='__main__':
+    main(sys.argv)
--- trunk/minds/weblib/test/test_graph.py
+++ trunk/minds/weblib/test/test_graph.py
@@ -1,6 +1,6 @@
 import unittest
 
-from minds.weblib import util
+from minds.weblib import graph
 
 
 class TestIndentedTextParsing(unittest.TestCase):
@@ -17,41 +17,107 @@ class TestIndentedTextParsing(unittest.TestCase):
                 C211
         C3
             C31
+            C1
     """
 
-    def test0(self):
-        tree = graph.build_indented_text_DAG('')
-        self.assertEqual(tree, ('',[]))
+    DEFORMED = """
+C1
+  C11
+  C12
+     C121
+    C122x
+ C13x
+  C131
+"""
 
-        tree = graph.build_indented_text_DAG('\n')
-        self.assertEqual(tree, ('',[]))
+    def test_rename(self):
+        # blank tests
+        self._check_rename('', '', '',[])
+        self._check_rename('', 'C1', 'X1',[])
+        self._check_rename('\n', 'C1', 'X1',[])
+
+        self._check_rename(self.DATA, 'C1', 'X1',[1,12])    # 2 renamed
+        self._check_rename(self.DATA, 'c1', 'X1',[1,12])    # lower case also OK
+        self._check_rename(self.DATA, 'c12', 'abc',[3])     # 1 renamed
+        self._check_rename(self.DATA, 'C211', 'def',[9])    # 1 renamed
+
+        self._check_rename(self.DATA, 'XXX', 'YYY',[])      # non-exist tag
+
+
+    def _check_rename(self, DATA, tag0, tag1, expected_diff):
+        edited = graph.edit_text_tree_rename(DATA, tag0, tag1)
+
+        # this is simple test, assume not 2 word tags
+        edited_words = edited.lower().split()
+        data_words = DATA.lower().split()
+        ltag0 = tag0.lower()
+        ltag1 = tag1.lower()
+
+        self.assertTrue(ltag0 not in edited_words)
+        if ltag0 in data_words:
+            self.assertTrue(ltag1 in edited_words)
+        self.assertEqual(len(DATA.splitlines()), len(edited.splitlines()))
+
+        # check line diff
+        diff = []
+        for i,line1,line2 in zip(xrange(9999), DATA.splitlines(), edited.splitlines()):
+            if line1 != line2:
+                diff.append(i)
+        self.assertEqual(expected_diff, diff)
+
+
+    def test0(self):
+        tree = graph.parse_text_tree('')
+        self.assertEqual(tree, ['',[]])
+
+        tree = graph.parse_text_tree('\n')
+        self.assertEqual(tree, ['',[]])
+
 
     def test_simple(self):
-        tree = graph.build_indented_text_DAG(self.DATA)
-        expected = ('', [
-        ('C1', [
-            ('C11',[]),
-            ('C12',[
-                ('C121',[]),
-                ('C122',[]),
-            ]),
-            ('C13',[]),
-        ]),
-        ('C2', [
-            ('C21', [
-                ('C211',[]),
-            ]),
-        ]),
-        ('C3', [
-            ('C31',[]),
-        ]),
-        ])
+        tree = graph.parse_text_tree(self.DATA)
+        expected = ['', [
+        ['C1', [
+            ['C11',[]],
+            ['C12',[
+                ['C121',[]],
+                ['C122',[]],
+            ]],
+            ['C13',[]],
+        ]],
+        ['C2', [
+            ['C21', [
+                ['C211',[]],
+            ]],
+        ]],
+        ['C3', [
+            ['C31',[]],
+            ['C1',[]],
+        ]],
+        ]]
+        self.assertEqual(tree, expected)
+
+
+    def test_deformed(self):
+        tree = graph.parse_text_tree(self.DEFORMED)
+        expected = ['', [
+        ['C1', [
+            ['C11',[]],
+            ['C12',[
+                ['C121',[]],
+                ['C122x',[]],   # child of C12, although it has smaller indent than sibling C121
+            ]],
+            ['C13x',[           # child of C1, although it has smaller indent than sibling C12
+                ['C131',[]],    # C131 is child of C13x, although it has same indent as uncle C12
+            ]],
+        ]],
+        ]]
         self.assertEqual(tree, expected)
 
 
     def test_dfs(self):
-        tree = graph.build_indented_text_DAG(self.DATA)
-        result = [(node[0], level) for node,level in util.dfs(tree)]
+        tree = graph.parse_text_tree(self.DATA)
+        result = list(graph.dfs(tree))
         expected = [
             ('',0),
             ('C1',1),
@@ -65,6 +131,7 @@ class TestIndentedTextParsing(unittest.TestCase):
             ('C211',3),
             ('C3',1),
             ('C31',2),
+            ('C1',2),
         ]
         self.assertEqual(result, expected)
 

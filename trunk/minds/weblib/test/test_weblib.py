37f620884627e12fe297509f5b6888db38c99d80
tungwaiyip <tungwaiyip@785ff9d5-dded-0310-b5f2-a5aff206d990>
2006-04-19 15:50:28 +0000

    added TestWebPage

--- trunk/minds/weblib/test/test_weblib.py
+++ trunk/minds/weblib/test/test_weblib.py
@@ -11,6 +11,58 @@ from minds.weblib import store
 
 testpath = testcfg.getpath('testDoc')
 
+class TestWebPage(unittest.TestCase):
+
+    def test_object(self):
+        w = weblib.WebPage(
+            timestamp   ='2000-01-01',
+            version     =7,
+            name        ='_name_',
+            nickname    ='_nickname_',
+            url         ='_url_',
+            description ='_description_',
+            tags        =[1],
+            created     ='2000-01-01',
+            modified    ='2000-01-02',
+            lastused    ='2000-01-03',
+            fetched     ='2000-01-04',
+            flags       ='_flags_',
+        )
+        w.tags_description = '1'
+
+        # test constructor has initialized all fields
+        self.assertEqual(w.timestamp  , '2000-01-01')
+        self.assertEqual(w.version    , 7)
+        self.assertEqual(w.name       , '_name_')
+        self.assertEqual(w.nickname   , '_nickname_')
+        self.assertEqual(w.url        , '_url_')
+        self.assertEqual(w.description, '_description_')
+        self.assertEqual(w.tags       , [1])
+        self.assertEqual(w.created    , '2000-01-01')
+        self.assertEqual(w.modified   , '2000-01-02')
+        self.assertEqual(w.lastused   , '2000-01-03')
+        self.assertEqual(w.fetched    , '2000-01-04')
+        self.assertEqual(w.flags      , '_flags_')
+        self.assertEqual(w.tags_description, '1')
+
+        # test __copy__() has copied all fields
+        w1 = w.__copy__()
+        self.assertEqual(w1.timestamp  , '2000-01-01')
+        self.assertEqual(w1.version    , 7)
+        self.assertEqual(w1.name       , '_name_')
+        self.assertEqual(w1.nickname   , '_nickname_')
+        self.assertEqual(w1.url        , '_url_')
+        self.assertEqual(w1.description, '_description_')
+        self.assertEqual(w1.tags       , [1])
+        self.assertEqual(w1.created    , '2000-01-01')
+        self.assertEqual(w1.modified   , '2000-01-02')
+        self.assertEqual(w1.lastused   , '2000-01-03')
+        self.assertEqual(w1.fetched    , '2000-01-04')
+        self.assertEqual(w1.flags      , '_flags_')
+        self.assertEqual(w1.tags_description, '1')
+
+
+
 class TestTag(unittest.TestCase):
 
     def test0(self):
